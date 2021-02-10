4ae515ed3ed6744401a3e6545212b17d7dd3e8b7
Jose Luis Leon <joseluis5000l@gmail.com>
2021-02-10 17:44:23 -0500

    Apply plugin after `java` plugin (#8)

--- plugin/src/main/groovy/com/github/joselion/strictnullcheck/StrictNullCheckPlugin.groovy
+++ plugin/src/main/groovy/com/github/joselion/strictnullcheck/StrictNullCheckPlugin.groovy
@@ -10,29 +10,33 @@ import org.gradle.api.Project
 import org.gradle.api.Plugin
 
 class StrictNullCheckPlugin implements Plugin<Project> {
+
+  @Override
   void apply(Project project) {
-    def ext = project.extensions.create(
-      'strictNullCheck',
-      StrictNullCheckExtension,
-      List.of('com.github.joselion.strictnullcheck.StrictNullPackage'),
-      "$project.buildDir/generated".toString(),
-      '3.0.2'
-    )
+    project.pluginManager.withPlugin('java', {
+      def ext = project.extensions.create(
+        'strictNullCheck',
+        StrictNullCheckExtension,
+        List.of('com.github.joselion.strictnullcheck.StrictNullPackage'),
+        "$project.buildDir/generated".toString(),
+        '3.0.2'
+      )
 
-    Configuration configuration = project.getConfigurations().getByName('compileOnly')
-    configuration.getDependencies().add(
-      project.getDependencies().create("com.google.code.findbugs:jsr305:$ext.findbugsVersion")
-    )
+      Configuration configuration = project.getConfigurations().getByName('compileOnly')
+      configuration.getDependencies().add(
+        project.getDependencies().create("com.google.code.findbugs:jsr305:$ext.findbugsVersion")
+      )
 
-    project.sourceSets.main.java.srcDirs(ext.generatedDir, 'plugin/src/main/java')
+      project.sourceSets.main.java.srcDirs(ext.generatedDir, 'plugin/src/main/java')
 
-    project.tasks.create(
-      'generatePackageInfo',
-      GeneratePackageInfoTask,
-      ext.generatedDir,
-      ext.annotations
-    )
+      project.tasks.create(
+        'generatePackageInfo',
+        GeneratePackageInfoTask,
+        ext.generatedDir,
+        ext.annotations
+      )
 
-    project.tasks.classes.finalizedBy(project.tasks.generatePackageInfo)
+      project.tasks.classes.finalizedBy(project.tasks.generatePackageInfo)
+    })
   }
 }
