05f2c0ede3b3f06edace8c6378d5d2caaa42e9ca
Jose Luis Leon <joseluis5000l@gmail.com>
2021-02-10 18:55:14 -0500

    Fix StrictNullCheckExtension issue (#12)

--- plugin/src/main/groovy/com/github/joselion/strictnullcheck/StrictNullCheckPlugin.groovy
+++ plugin/src/main/groovy/com/github/joselion/strictnullcheck/StrictNullCheckPlugin.groovy
@@ -5,23 +5,24 @@ import groovy.text.SimpleTemplateEngine
 
 import java.util.List
 
-import org.gradle.api.artifacts.Configuration
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.plugins.JavaPlugin
 
 class StrictNullCheckPlugin implements Plugin<Project> {
 
   @Override
   void apply(Project project) {
-    project.pluginManager.withPlugin('java', {
-      def ext = project.extensions.create(
-        'strictNullCheck',
-        StrictNullCheckExtension,
-        List.of('com.github.joselion.strictnullcheck.StrictNullPackage'),
-        "$project.buildDir/generated".toString(),
-        '3.0.2'
-      )
+    def ext = project.extensions.create(
+      'strictNullCheck',
+      StrictNullCheckExtension,
+      List.of('com.github.joselion.strictnullcheck.StrictNullPackage'),
+      "$project.buildDir/generated".toString(),
+      '3.0.2'
+    )
 
+    project.plugins.withType(JavaPlugin) {
       Configuration configuration = project.getConfigurations().getByName('compileOnly')
       configuration.getDependencies().add(
         project.getDependencies().create("com.google.code.findbugs:jsr305:$ext.findbugsVersion")
@@ -37,6 +38,6 @@ class StrictNullCheckPlugin implements Plugin<Project> {
       )
 
       project.tasks.classes.finalizedBy(project.tasks.generatePackageInfo)
-    })
+    }
   }
 }
